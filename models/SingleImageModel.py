f350eb9a86e6d2d2cb9f3e140786de8d8cd34904
padmanabhankrishnamurthy <pkaa@connect.ust.hk>
2022-03-15 15:22:31 -0400

    added single image model

---
+++ models/SingleImageModel.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+import torchvision.models as models
+
+class SingleImageModel(nn.Module):
+
+    def __init__(self):
+        '''
+            param device: set device to 'cpu' or 'cuda', default 'cpu'
+        '''
+        super(SingleImageModel, self).__init__()
+
+        self.image_encoder = self.get_image_encoder()
+        self.prediction_head = self.get_prediction_head()
+        
+        self.image_encoder.fc = self.prediction_head
+
+    def get_image_encoder(self):
+        '''
+            Use ResNet's feature extractors as the image encoder
+        '''
+        resnet = models.resnet18(pretrained=True)
+        return resnet
+
+    def get_prediction_head(self):
+        prediction_head = nn.Linear(in_features=512, out_features=1, bias=True)
+        return prediction_head
+
+    def forward(self, x):
+        x = self.image_encoder(x)
+        return x
+
+    
+
+    
---
+++ models/train_single_image.py
@@ -0,0 +1,46 @@
+import sys
+sys.path.insert(0, '..')
+
+import torch 
+import torch.nn as nn
+import numpy as np
+
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+from data_utils.misc import norm_tensor_to_img
+from data_utils.SingleImageDataset import SingleImageDataset
+from models.SingleImageModel import SingleImageModel
+
+if __name__ == '__main__':
+    train_file = '../data/Train.csv'
+    image_dir = '../data/Train'
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    epochs = 10
+
+    dataset = SingleImageDataset(train_file=train_file, image_dir=image_dir)
+    train_loader = DataLoader(dataset=dataset, batch_size=1, shuffle=True)
+    model = SingleImageModel().to(device)
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+    criterion = nn.L1Loss()
+
+    for epoch in range(epochs):
+        epoch_loss = 0
+        pbar = tqdm(train_loader)
+
+        for batch in pbar:
+            # every item in batch: (image, percentage, patient_id, filename)
+            optimizer.zero_grad()
+
+            images = batch[0]
+            percentages = batch[1]
+
+            predictions = model(images)
+            batch_loss = criterion(percentages, predictions)
+            epoch_loss+=batch_loss
+
+            batch_loss.backward()
+            optimizer.step()
+            pbar.set_description('Epoch: {} Loss: {}'.format(epoch + 1, batch_loss))
+
+
